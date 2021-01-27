d741848dc15f6617965d26ba30487fd9676325c1
duongkstn <nguyenduongyht@gmail.com>
2021-01-27 11:33:35 +0700

    ver

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from rasalit import __version__
 base_packages = [
     "streamlit>=0.57.3",
     "pyyaml>=5.3.1",
-    "pandas>=1.0.3",
+    # "pandas>=1.0.3",
     "altair>=4.1.0",
     "typer>=0.3.0",
-    "rasa>=2.0",
-    "spacy>=2.3.2",
-    "tensorflow>=2.3.1",
+    # "rasa>=2.0",
+    # "spacy>=2.3.2",
+    # "tensorflow>=2.3.1",
 ]
 
 dev_packages = ["flake8>=3.6.0", "pytest>=4.0.2", "pre-commit>=2.7.1", "black"]
